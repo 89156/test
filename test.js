33a9b826105821f067fccfb975596bb7a1ac6b19
89156 <Asteam_89156@outlook.com>
2025-11-18 01:19:13 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         天才们的邀约 正确答案高亮助手
 // @namespace    https://starrailawards.com/
-// @version      0
+// @version      1
 // @description  适用于⌈天才们的邀约⌋小游戏
 // @match        *://geniuses.starrailawards.com/*
 // @grant        none
@@ -11,8 +11,7 @@
 'use strict';
 let DB={};
 fetch("https://gh-proxy.org/https://raw.githubusercontent.com/89156/userjs/refs/heads/main/quiz_db.json")
-.then(r=>r.json())
-.then(j=>{DB=j});
+.then(r=>r.json()).then(j=>{DB=j});
 
 const buildOptKey=o=>{
     let k=o.innerText.trim();
@@ -23,7 +22,7 @@ const buildOptKey=o=>{
 };
 
 const addHint=q=>{
-    if(q.dataset.hinted) return;
+    if(q.dataset.hinted)return;
     const t=q.innerText.trim();
     if(t==="这段声音是来自谁的？请找出与题目语音同角色的选项"){
         const hint=document.createElement('div');
@@ -37,19 +36,20 @@ const addHint=q=>{
 const highlight=()=>{
     const q=document.querySelector('.question-text');
     if(!q)return;
+    addHint(q);
     let key=q.innerText.trim();
+    if(key.includes('请找出与题目语音同角色的选项'))return;
     const box=q.closest('.question-container');
     if(box){
         const s=box.querySelector('audio source'),i=box.querySelector('img');
         if(s)key+=`|audio:${s.src}`;
         if(i)key+=`|img:${i.src}`;
     }
-    if(key.includes('请找出与题目语音同角色的选项'))return;
     const ops=[...document.querySelectorAll('.option')];
     ops.forEach(o=>{o.style.border=''});
-    const match=DB[key];
-    if(match){
-        const hit=ops.find(o=>buildOptKey(o)===match);
+    const ans=DB[key];
+    if(ans){
+        const hit=ops.find(o=>buildOptKey(o)===ans);
         if(hit)hit.style.border='5px solid lime';
     }
 };
